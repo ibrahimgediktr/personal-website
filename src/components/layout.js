6b20d54a779ad3719f5e3a61531b93c066566723
İbrahim Gedik <67916168+ibrahimgediktr@users.noreply.github.com>
2020-12-19 20:50:55 +0300

    update header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Footer from "./Footer"
-import Header from "./Header"
+import Header from "./header"
+
 import GlobalStyle from "./styles/GlobalStyles"
 
 const Layout = ({ children }) => {
