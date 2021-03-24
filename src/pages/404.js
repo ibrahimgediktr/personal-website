8356e45fcda0abd164e4f86a6b036d9f3bf36942
İbrahim Gedik <67916168+ibrahimgediktr@users.noreply.github.com>
2021-03-24 19:29:41 +0300

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,10 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found | İbrahim Gedik" />
+    <div style={{display:"flex", justifyContent:"center", alignItems:"center" , flexDirection:"column", minHeight:"100vh"}}>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 )
 
